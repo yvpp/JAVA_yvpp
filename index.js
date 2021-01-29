d76053d41d19e8e819d5f743c61581f5d4a5a9e7
yvpp <yvpp@yahoo.com>
2021-01-29 14:38:03 -0600

    Ejercicio 1.1

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const areaBase = 16*23;
 const areaTotal = areaLateral + 2*areaBase;
 console.log('areaTotal', areaTotal+'cm2');
 
+// ESTANDAR
+const base = 10;
+const altura = 15;
+const ancho = 5;
+
+const volumen2 = base * altura * ancho;
+console.log('volumen2', volumen2);
+
+const area2 = base*altura+2*base*ancho;
+console.log('area2', area2);
+
 
 
 
