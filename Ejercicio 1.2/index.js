a46d1d53a11935cf77ed55ee24053c25096693e1
yvpp <yvpp@yahoo.com>
2021-01-29 15:06:49 -0600

    Ejercicio 1.2

---
+++ Ejercicio 1.2/index.js
@@ -0,0 +1,9 @@
+
+const yvpp = {
+    day: 10,
+    month: 'Agosto'
+};
+console.log('Mi cumpleaños es el ' + yvpp.day + ' de' + yvpp.month);
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+console.log(meses[8]);
