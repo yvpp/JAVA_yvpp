6e963f60725e055819de8f4bbacc046cbe7cd772
yvpp <yvpp@yahoo.com>
2021-01-29 14:25:11 -0600

    Ejercicio 1.1

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Prueba');
+// base 16 * 23 cm
+// altura 13 cm
+
+const volumen = 16 * 23 * 13;
+console.log('volumen', volumen + 'cm3');
+
+// A = al+2ab
+const areaLateral = 23*13;
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log('areaTotal', areaTotal+'cm2');
+
+
+
+
