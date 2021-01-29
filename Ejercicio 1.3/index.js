ebcfdca07c6db079f157b64e0c22a76efba601ce
yvpp <yvpp@yahoo.com>
2021-01-29 15:40:14 -0600

    Ejercicio 1.3

---
+++ Ejercicio 1.3/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const data = ['hola', 2, 5, 'adiós'];
+
+// Determina cuál de las dos cadenas que hay en el array es mayor.
+console.log('5 es mayor que 2', data[2]>data[1]);
+
+// Operaciones numéricas
+console.log('Suma', data[1]+data[2]);
+console.log('Resta', data[1]-data[2]);
+console.log('Multiplicación', data[1]*data[2]);
+console.log('División', data[1]/data[2]);
+console.log('Cociente', data[1]%data[2]);
